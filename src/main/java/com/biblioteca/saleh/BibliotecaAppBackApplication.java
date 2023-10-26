e804ed44c654aedc5dbb822a0cdaeec1081ebb1a
MatheusSaleh <matheusxmg2@gmail.com>
2023-10-25 21:12:52 -0300

    Commit Inicial

---
+++ src/main/java/com/biblioteca/saleh/BibliotecaAppBackApplication.java
@@ -0,0 +1,13 @@
+package com.biblioteca.saleh;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BibliotecaAppBackApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BibliotecaAppBackApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/biblioteca/saleh/BibliotecaAppBackApplicationTests.java
@@ -0,0 +1,13 @@
+package com.biblioteca.saleh;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BibliotecaAppBackApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
